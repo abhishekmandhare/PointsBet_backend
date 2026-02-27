e33b435d854661e55a4ca0b8d7abc7b941ad2107
Abhishek Mandhare <abhishek.mandhare@yahoo.com>
2026-02-27 13:25:54 +1100

    Adding repo to github

---
+++ PointsBet/StringFormatter.cs
@@ -0,0 +1,32 @@
+﻿namespace PointsBet.Backend.Online.Code.Test
+{
+
+    /*
+    Improve a block of code as you see fit in C#.
+    You may make any improvements you see fit, for example:
+      - Cleaning up code
+      - Removing redundancy
+      - Refactoring / simplifying
+      - Fixing typos
+      - Any other light-weight optimisation
+    */
+    public class StringFormatter
+    {
+        // Making following assumptions:
+        // if quote is null, it will be empty string ""
+        // if items array is null, we cannot continue and it will throw argument exception.
+        // if items array is empty, it wll return empty string. I think this is less aggresive than throwing exception and caller will not have to additional handle errors.
+        // if one of the elements in items array is null empty or just spaces, it will skip over that element and continue with the rest. Reason having an empty , , doesn't make much sense. Other option is to fail the whole function, which to me is excessive and limiting.
+
+        // Improvements
+        // 1. Using Linq (select, Where) instead of loop for readability.
+        // 2. Fixed typo in function name
+        // 3. Fixed edge cases of empty elements
+        // 4. Removed redundancy and simplified.
+        public static string ToCommaSeparatedList(string[] items, string? quote)
+        {
+            ArgumentNullException.ThrowIfNull(items);
+            return string.Join(", ",  items.Where(item => !string.IsNullOrWhiteSpace(item)).Select(item => $"{quote}{item}{quote}"));
+        }
+    }
+}
---
+++ PointsBetTest/StringFormatterTest.cs
@@ -0,0 +1,34 @@
+﻿using PointsBet.Backend.Online.Code.Test;
+
+namespace PointsBet
+{
+    [TestClass]
+    public class StringFormatterTest
+    {
+        [TestMethod]
+        [DataRow("`1`, `2`, `3`", new string[] { "1", "2", "3" }, "`")]
+        [DataRow("1, 2, 3", new string[] { "1", "2", "3" }, null)]
+        [DataRow(@"""1"", ""2"", ""3""", new string[] { "1", "2", "3" }, @"""")]
+        [DataRow("", new string[] { }, "`")]
+        [DataRow("1", new String[] { "1" }, "")]
+        [DataRow("1, 2", new string[] { "1", null!, "2" }, "")]
+        [DataRow("", new string[] { null!, null!, null! }, "")]
+        [DataRow("!2!, !3!", new string[] { " ", "2", "3" }, "!")]
+        [DataRow("!1!", new string[] { "1", "", "" }, "!")]
+        public void ToCommaSeparatedListTest(string expected, string[] listInput, string quoteInput)
+        {
+            var actual = StringFormatter.ToCommaSeparatedList(listInput, quoteInput);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToCommaSeparatedListTest_NullItems_ThrowsException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => StringFormatter.ToCommaSeparatedList(null!, "!"));
+
+            Assert.AreEqual("items", exception.ParamName);
+        }
+
+    }
+}
